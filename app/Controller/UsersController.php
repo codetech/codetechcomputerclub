d314f046fa2a092735b86512bade5988582d9708
jonjmz <jonathanjimenezromo@gmail.com>
2014-02-05 16:38:44 -0800

    relocated email sending method

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -14,7 +14,7 @@ class UsersController extends AppController {
  *
  * @var array
  */
-	public $components = array('Paginator', 'Picturesque');
+	public $components = array('Paginator', 'Picturesque', 'Email');
 
 /**
  * Helpers
@@ -268,77 +268,18 @@ class UsersController extends AppController {
  */
 	public function send_email() {
 		if ($this->request->is('post')) {
-			
-			// Grab all email addressed which have opted to receive emails.
-			$emails = $this->User->find('list', array(
-				'conditions' => array('User.receiveemail' => true),
-				'fields' => array('User.email')
-			));
-			
-			// Split the emails list so that there will be one "to" address
-			// and the rest will be bccs.
-			$firstEmail = array_shift($emails);
-			$Email = new CakeEmail();
-			$Email->config('gmail')
-				->template('default', null)
-				->emailFormat('both')
-				->to($firstEmail)
-				->bcc($emails)
-				->subject($this->request->data['Email']['title'])
-				->viewVars(array(
-					'textContent' => $this->request->data['Email']['text_message'],
-					'htmlContent' => $this->request->data['Email']['html_message'],
-				))
-				->send();
-			
-			$this->Session->setFlash('The email has been sent.');
+			$this->Email->sendEmail(
+				$this->request->data['Email']['title'],
+				$this->request->data['Email']['html_message'],
+				$this->request->data['Email']['text_message']);
+			$this->Session->setFlash('The emails have been sent.');
 			return $this->redirect(array('action' => 'index'));
 		}
 	}
 	
 	public function send_text() {
 		if ($this->request->is('post')) {
-			
-			// Select only relevant phone and address information.
-			$numbers = $this->User->find('all', array(
-				'conditions' => array(
-					'User.receivesms' => true,
-					'User.phone !=' => ''
-				),
-				'fields' => array('User.phone'),
-				'contain' => array('Gateway.address'),
-			));
-			
-			// Generate SMS email addresses.
-			// To prevent abuse, only send up to 5 texts for each User.
-			// Counting the texts probably isn't necessary now that limits
-			// are imposed at the Model level. Remove that check when things
-			// feel safe.
-			$emails = array();
-			foreach ($numbers as $number) {
-				$pureNumber = preg_replace('/[^0-9]/', '', $number['User']['phone']);
-				$textsGenerated = 0;
-				foreach ($number['Gateway'] as $gateway) {
-					if ($textsGenerated === 5) break;
-					array_push($emails, $pureNumber . '@' . $gateway['address']);
-					$textsGenerated++;
-				}
-			}
-			
-			// Split the emails list so that there will be one "to" address
-			// and the rest will be bccs.
-			$firstEmail = array_shift($emails);
-			$Email = new CakeEmail();
-			$Email->config('gmail')
-				->template('default', null)
-				->emailFormat('text')
-				->to($firstEmail)
-				->bcc($emails)
-				->viewVars(array(
-					'textContent' => $this->request->data['Email']['text_message'],
-				))
-				->send();
-			
+			$this->Email->sendText($this->request->data['Email']['text_message']);
 			$this->Session->setFlash('The text has been sent.');
 			return $this->redirect(array('action' => 'index'));
 		}
